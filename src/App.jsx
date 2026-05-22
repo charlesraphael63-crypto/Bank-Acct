b9295d6366e8392422ded8839ee0025b57c78ef9
Raphael Oluwapelumi john <charlesraphael63-crypto@example.com>
2026-05-22 11:23:09 +0100

    I work with backend for users to create acct and sign in

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,16 @@ import LandingPage from "./Pages/LandingPage";
 import LoginPage from "./Auth/LoginPage";
 import ErrorPage from "./Pages/ErrorPage";
 import SignUpPage from "./Auth/SignUpPage";
+import PrivateRouting from "./Lib/PrivateRouting";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<ErrorPage />} />
-        <Route path="landing-page" element={<LandingPage />} />
+        <Route element={<PrivateRouting />}>
+          <Route path="landing-page" element={<LandingPage />} />
+        </Route>
         <Route path="signup-page" element={<SignUpPage />} />
         <Route path="/" element={<LoginPage />} />
       </Routes>
